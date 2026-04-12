bf91f092d11127591219bf28960b690265192ae3
devdilshan2025 <dsbandara4@gmail.com>
2026-04-12 18:43:57 +0530

    Recorrected app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Slider එකට අනිවාර්යයි
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
